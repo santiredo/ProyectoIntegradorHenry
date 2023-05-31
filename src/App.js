6c23c6abe98973209652e2772c4c71011b84bbe1
santiRedondo <santi16redo@gmail.com>
2023-05-31 19:39:33 -0300

    Modificando la funcion onSearch para que acepte otros valores de busqueda

--- src/App.js
+++ src/App.js
@@ -8,17 +8,56 @@ export default function App() {
    const [characters, setCharacters] = useState([]);
 
    function onSearch(id) {
-      axios(`https://rickandmortyapi.com/api/character/${id}`).then(({ data }) => {
-         if (data.name) {
+      axios(`https://rickandmortyapi.com/api/character`).then(({ data }) => {
+
+
+         const filteredCharacters = [];
+         for (const char of data.results){
+            const values = Object.values(char);
+            let isRendered = false;
+
+            for (const character of characters){
+               if(character.id === char.id){
+                  isRendered = true;
+                  break;
+               }               
+            }
+
+            if(!isRendered){
+
+               for (let value of values) {
+                  if(typeof value === 'string'){
+                     if(value.toLowerCase() === id.toLowerCase()){
+
+                        filteredCharacters.push(char);
+                        break;
+                     }
+                  } else if (typeof value === 'number'){
+                     if(value === parseInt(id)){
+                        filteredCharacters.push(char);
+                        break;
+                     }
+                  }
+                  
+               }
+            }
+         }
+         if(filteredCharacters.length === 0){
+            window.alert('¡No hay personajes con este ID!');
+         } else {
+            setCharacters((oldChars) => [...oldChars, ...filteredCharacters]);
+         }
+
+/*          if (data.name) {
             setCharacters((oldChars) => [...oldChars, data]);
          } else {
             window.alert('¡No hay personajes con este ID!');
-         }
+         } */
       });
    }
 
    function onClose(id){
-      setCharacters(characters.filter(char => char.id !== parseInt(id)))
+      setCharacters((oldChars) => oldChars.filter((char) => char.id !== id));
    }
 
    return (
