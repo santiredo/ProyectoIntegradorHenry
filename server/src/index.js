cdcefa31240d6c2fbc5cfbd76d72f0ed2282a1b9
santiRedondo <santi16redo@gmail.com>
2023-06-28 15:41:25 -0300

    Creando base de datos

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,10 @@
 const server = require('./app')
 const PORT = 3002;
+const { conn } = require('./DB_connection');
+
 
 
 server.listen(PORT, () => {
+    conn.sync({force:true})
     console.log('Server raised un port: ' + PORT)
 })
