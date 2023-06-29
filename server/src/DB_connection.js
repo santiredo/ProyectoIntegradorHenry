5b307367708a52d6fd071ac5b8b1c562409e002b
santiRedondo <santi16redo@gmail.com>
2023-06-29 14:20:02 -0300

    Configurando unos errores de dotenv

--- server/src/DB_connection.js
+++ server/src/DB_connection.js
@@ -4,26 +4,16 @@ const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 const UserModel = require('./models/User');
 const FavoriteModel = require('./models/Favorite');
 
-// EJERCICIO 03
-// A la instancia de Sequelize le falta la URL de conexión. ¡Agrégala!
-// Recuerda pasarle la información de tu archivo '.env'.
 
-// URL ----> postgres://DB_USER:DB_PASSWORD@DB_HOST/rickandmorty
 const sequelize = new Sequelize(
-   /* URL: */ `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
+   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/rickandmorty`,
    { logging: false, native: false }
 );
 
-// EJERCICIO 05
-// Debajo de este comentario puedes ejecutar la función de los modelos.
 
 UserModel(sequelize);
 FavoriteModel(sequelize);
 
-//
-
-// Ejercicio 06
-// ¡Relaciona tus modelos aquí abajo!
 const { User, Favorite } = sequelize.models;
 
 User.belongsToMany(Favorite, {through: 'user_favorite'})
